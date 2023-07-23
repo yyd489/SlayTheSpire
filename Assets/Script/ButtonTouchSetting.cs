89011c674d250933eba5e2d9ff3e9efe5176756a
병준 <yyd489@gmail.com>
2023-07-23 17:03:15 +0900

    플레이어 캐릭터 공격 수정, 인게임 UI 추가, 버튼 이미지공백 터치막는 스크립트 추가

---
+++ Assets/Script/ButtonTouchSetting.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTouchSetting : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
