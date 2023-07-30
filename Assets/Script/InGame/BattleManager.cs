6fb9d080a3bd4d2129066edeebea9975a22df78b
병준 <yyd489@gmail.com>
2023-07-30 16:14:18 +0900

    플레이어 카드 연출 작업_1

--- Assets/Script/InGame/BattleManager.cs
+++ Assets/Script/InGame/BattleManager.cs
@@ -1,21 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Cysharp.Threading.Tasks;
+using System;
 
 namespace FrameWork
 {
+    enum BattleState
+    {
+        Ready,
+        PlayerTurn,
+        EnemyTurn,
+        EndBattle
+    }
+
     public class BattleManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        private BattleState battleState;
+        private int maxEnergy;
+        private int energy;
+        private bool isPlayerControl;
+
+        public CharacterBase playerCharacter;
+        public List<CharacterBase> enemyCharacters;
+
+        [SerializeField] private TextMeshProUGUI energyText;
+        [SerializeField] GameObject TurnEndBtn;
+
+
         void Start()
         {
-
+            maxEnergy = 3;
+            energy = maxEnergy;
+            isPlayerControl = false;
+            battleState = BattleState.Ready;
+            TurnChange();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void TurnChange()
         {
+            switch (battleState)
+            {
+                case BattleState.Ready:
+                    battleState = BattleState.PlayerTurn;
+                    break;
+                case BattleState.PlayerTurn:
+                    battleState = BattleState.EnemyTurn;
+                    EnemyTurn();
+                    break;
+                case BattleState.EnemyTurn:
+                    battleState = BattleState.PlayerTurn;
+                    break;
+                case BattleState.EndBattle:
+                    break;
+            }
 
+            if(battleState == BattleState.PlayerTurn)
+            {
+                TurnEndBtn.SetActive(true);
+                energy = maxEnergy;
+                energyText.text = energy + "/" + maxEnergy;
+            }
+            else
+            {
+                TurnEndBtn.SetActive(false);
+            }
+        }
+
+        // 테스트용---------------------------------------------------------------------------------------------------------
+        public async void PlayerAttack()
+        {
+            if (energy > 0 && !isPlayerControl && battleState == BattleState.PlayerTurn)
+            {
+                isPlayerControl = true;
+                energy--;
+                energyText.text = energy + "/" + maxEnergy;
+                await playerCharacter.Attack(enemyCharacters[0]);
+                isPlayerControl = false;
+            }
+        }
+
+        private async UniTask EnemyTurn()
+        {
+            TimeSpan delayTime = TimeSpan.FromSeconds(1);
+            for (int i = 0; i < enemyCharacters.Count; i++)
+            {
+                await UniTask.Delay(delayTime);
+                await enemyCharacters[i].Attack(playerCharacter);
+                await UniTask.Delay(TimeSpan.FromSeconds(1.5f));
+            }
+
+            await UniTask.Delay(delayTime);
+            TurnChange();
         }
     }
 }
---
+++ Assets/Script/UI/CardSorting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+namespace FrameWork
+{
+    public class CardSorting : MonoBehaviour
+    {
+        [SerializeField] List<CardBase> cards;
+
+        void Start()
+        {
+            DefaltCardSorting();
+        }
+
+        public void DefaltCardSorting()
+        {
+            int cardCount = cards.Count;
+            int halfCount = (int)(cardCount * 0.5f);
+
+            for (int i = 0; i < cardCount; i++)
+            {
+                cards[i].cardIndex = i;
+                int tempInt = i - halfCount;
+                float xPos = tempInt * 180f;
+                float yPos = 0f;
+
+                cards[i].transform.rotation = Quaternion.identity;
+                cards[i].transform.Rotate(new Vector3(0f, 0f, 0f - tempInt * 5f));
+
+                if (tempInt < 0) tempInt *= -1;
+
+                for (int j = 1; j <= tempInt; j++)
+                {
+                    yPos -= j * 15f;
+                }
+
+                cards[i].transform.localPosition = new Vector2(xPos, yPos);
+            }
+        }
+
+        public void SelectCard(CardBase selectCard)
+        {
+            int cardIndex = selectCard.cardIndex;
+
+            if (cardIndex > 0)
+            {
+                Vector3 movePos = cards[cardIndex - 1].transform.localPosition - new Vector3(50f, 0f);
+                cards[cardIndex - 1].transform.DOLocalMove(movePos, 0.5f);
+            }
+
+            if(cardIndex + 1 <= cards.Count )
+            {
+                Vector3 movePos = cards[cardIndex + 1].transform.localPosition + new Vector3(50f, 0f);
+                cards[cardIndex + 1].transform.DOLocalMove(movePos, 0.5f);
+            }
+        }
+
+    }
+}
