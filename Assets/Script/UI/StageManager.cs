02861eae9c82cf8aaefe42c52cd5ea10e2e504f5
오종택 <ejjt258@rp5entartainment.com>
2023-10-31 16:46:29 +0900

    스테이지매니저 코드수정

--- Assets/Script/UI/StageManager.cs
+++ Assets/Script/UI/StageManager.cs
@@ -21,72 +21,80 @@ namespace FrameWork
           public GameObject shopPop;
           public GameObject restPop;
           public GameObject[] arrEvents;
+          private System.Action[] arrStage = new System.Action[6];
+          private System.Action<Data.CharacterData>[] arrEvent = new System.Action<Data.CharacterData>[3];
+
+        public void init()
+        {
+            arrStage[(int)MapField.Event] = OnEventStage;
+            arrStage[(int)MapField.Monster] = OnMonsterStage;
+            arrStage[(int)MapField.EliteMonster] = OnEliteMonsterStage;
+            arrStage[(int)MapField.Shop] = OnShopStage;
+            arrStage[(int)MapField.Sleep] = OnSleepStage;
+            arrStage[(int)MapField.Boss] = OnBossStage;
+
+            
+            arrEvent[(int)FieldEvenets.GoldEvent] = OnGoldEvent;
+            arrEvent[(int)FieldEvenets.FireEvenet] = OnFireEvenet;
+            arrEvent[(int)FieldEvenets.ClericEvent] = OnClericEvent;
+        }
 
         public void ControlField(MapField fieldInfo)
         {
             GameManager.Instance.battleManager.stage = fieldInfo;
-            switch (fieldInfo)
-            {
+            arrStage[(int)fieldInfo]();
+         
+        }
 
-                case MapField.Event:
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
-                    int randomEvent = Random.Range(0, arrEvents.Length);
-                    Transform eventObj = Instantiate(arrEvents[randomEvent].transform);
-                    eventObj.Find("EffectButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(eventObj.gameObject));
-                    eventObj.Find("EffectButton").GetComponent<Button>().onClick.AddListener(() => ActiveEvent((FieldEvenets)randomEvent));
-                    ControlEventText(eventObj, randomEvent);
+        public void OnEventStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
+            int randomEvent = Random.Range(0, arrEvents.Length);
+            Transform eventObj = Instantiate(arrEvents[randomEvent].transform);
+            eventObj.Find("EffectButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(eventObj.gameObject));
+            eventObj.Find("EffectButton").GetComponent<Button>().onClick.AddListener(() => ActiveEvent((FieldEvenets)randomEvent));
+            ControlEventText(eventObj, randomEvent);
+        }
 
-                    break;
+        public void OnMonsterStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
+            GameManager.Instance.spawnManager.Init(MapField.Monster);
+            GameManager.Instance.battleManager.Init();
+            GameManager.Instance.cardManager.StageStart();
+        }
 
-                case MapField.Monster:
+        public void OnEliteMonsterStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
+            GameManager.Instance.spawnManager.Init(MapField.EliteMonster);
+            GameManager.Instance.battleManager.Init();
+            GameManager.Instance.cardManager.StageStart();
+        }
 
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
-                    GameManager.Instance.spawnManager.Init(MapField.Monster);
-                    GameManager.Instance.battleManager.Init();
-                    GameManager.Instance.cardManager.StageStart();
-                    break;
+        public void OnShopStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
+            GameObject shopObj = Instantiate(shopPop);
+            shopObj.transform.Find("Canvas").Find("NextButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(shopObj));
+        }
 
-                case MapField.EliteMonster:
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
-                    GameManager.Instance.spawnManager.Init(MapField.EliteMonster);
-                    GameManager.Instance.battleManager.Init();
-                    GameManager.Instance.cardManager.StageStart();
-                    break;
+        public void OnSleepStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
+            GameObject restObj = Instantiate(restPop);
+            restObj.transform.Find("RestButton").GetComponent<Button>().onClick.AddListener(() => FillHp());
+            restObj.transform.Find("RestButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(restObj));
+        }
 
-                case MapField.Shop:
-
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
-                    GameObject shopObj = Instantiate(shopPop);
-                    //shopObj.transform.Find("ShopPanel").Find("BackButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(shopObj));
-                    shopObj.transform.Find("Canvas").Find("NextButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(shopObj));
-
-
-
-                    break;
-
-                case MapField.Sleep:
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(false);
-                    GameObject restObj = Instantiate(restPop);
-                    restObj.transform.Find("RestButton").GetComponent<Button>().onClick.AddListener(() => FillHp());
-                    restObj.transform.Find("RestButton").GetComponent<Button>().onClick.AddListener(() => ClearStage(restObj));
-
-
-                    break;
-
-                case MapField.Boss:
-
-                   
-                    GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
-                    GameManager.Instance.spawnManager.Init(MapField.Boss);
-                    GameManager.Instance.soundManager.backgroundAudio.clip = GameManager.Instance.soundManager.backgroundSound[2];
-                    GameManager.Instance.soundManager.backgroundAudio.Play();
-                    GameManager.Instance.battleManager.Init();
-                    GameManager.Instance.cardManager.StageStart();
-                    break;
-
-            }
-
-
+        public void OnBossStage()
+        {
+            GameManager.Instance.initilizer.inGameUiCanvas.gameObject.SetActive(true);
+            GameManager.Instance.spawnManager.Init(MapField.Boss);
+            GameManager.Instance.soundManager.backgroundAudio.clip = GameManager.Instance.soundManager.backgroundSound[2];
+            GameManager.Instance.soundManager.backgroundAudio.Play();
+            GameManager.Instance.battleManager.Init();
+            GameManager.Instance.cardManager.StageStart();
         }
 
         public void ClearStage(GameObject targetObj = null)
@@ -100,67 +108,66 @@ namespace FrameWork
         public void FillHp()
         {
             int fillHp = 30;
-            int maxHp = GameManager.Instance.dataManager.data.characterData.GetCharacterStat().maxHp;
+            int maxHp = GameManager.Instance.dataManager.data.characterData.characterStat().maxHp;
             GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.hp += fillHp;
 
-            if (GameManager.Instance.dataManager.data.characterData.GetCharacterStat().hp > maxHp)
+            if (GameManager.Instance.dataManager.data.characterData.characterStat().hp > maxHp)
             {
                 GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.hp = maxHp;
             }
 
-            GameManager.Instance.ingameTopUI.nowHpText.text = GameManager.Instance.dataManager.data.characterData.GetCharacterStat().hp + "/" + GameManager.Instance.dataManager.data.characterData.GetCharacterStat().maxHp.ToString();
+            GameManager.Instance.ingameTopUI.nowHpText.text = GameManager.Instance.dataManager.data.characterData.characterStat().hp + "/" + GameManager.Instance.dataManager.data.characterData.characterStat().maxHp.ToString();
             GameManager.Instance.playerControler.ironclad.Heal(0);
         }
 
         public void ControlEventText( Transform eventTransform,int eventType)
         {
-            eventTransform.Find("EventTitleText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.GetEventData()[eventType].eventName;
-            eventTransform.Find("TitleBackGround").Find("EventText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.GetEventData()[eventType].eventGuide;
-            eventTransform.Find("EffectButton").Find("EffectText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.GetEventData()[eventType].effectText;
+            eventTransform.Find("EventTitleText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.eventData()[eventType].eventName;
+            eventTransform.Find("TitleBackGround").Find("EventText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.eventData()[eventType].eventGuide;
+            eventTransform.Find("EffectButton").Find("EffectText").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.dataManager.data.eventData.eventData()[eventType].effectText;
         }
 
         public void ActiveEvent(FieldEvenets eventType)
         {
-           
-
+      
             var characterData = GameManager.Instance.dataManager.data.characterData;
-            switch (eventType)
-            {
-                case FieldEvenets.GoldEvent:
-
-                    int rewardGold = 10;
-
-                    characterData.characterInfoCollect.characterCollect.gold += rewardGold;
-                    GameManager.Instance.ingameTopUI.goldText.text = characterData.characterInfoCollect.characterCollect.gold.ToString();
-                    break;
-
-                case FieldEvenets.FireEvenet:
-
-                    int fillHp = 30;
-                    characterData.characterInfoCollect.characterCollect.hp += fillHp;
-                    
-                    if (characterData.characterInfoCollect.characterCollect.hp > characterData.characterInfoCollect.characterCollect.maxHp)
-                    {
-                        characterData.characterInfoCollect.characterCollect.hp = characterData.characterInfoCollect.characterCollect.maxHp;
-
-                    }
-                    GameManager.Instance.ingameTopUI.nowHpText.text = characterData.characterInfoCollect.characterCollect.hp + "/" + characterData.characterInfoCollect.characterCollect.maxHp.ToString();
-                    GameManager.Instance.playerControler.ironclad.Heal(0);
-                    break;
-
-                case FieldEvenets.ClericEvent:
-
-                    int fillLittleHp = 10;
-
-                    GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.hp += fillLittleHp;
-                    GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.maxHp += fillLittleHp;
-                    GameManager.Instance.ingameTopUI.nowHpText.text = characterData.characterInfoCollect.characterCollect.hp + "/" + characterData.characterInfoCollect.characterCollect.maxHp.ToString();
-                    GameManager.Instance.playerControler.ironclad.Heal(0);
-                    break;
-            }
+            arrEvent[(int)eventType](characterData);
 
             GameManager.Instance.playerControler.ironclad.Heal(0);
         }
+
+
+        private void OnGoldEvent(Data.CharacterData characterData)
+        {
+            int rewardGold = 10;
+            characterData.characterInfoCollect.characterCollect.gold += rewardGold;
+            GameManager.Instance.ingameTopUI.goldText.text = characterData.characterInfoCollect.characterCollect.gold.ToString();
+        }
+
+        private void OnFireEvenet(Data.CharacterData characterData)
+        {
+            int fillHp = 30;
+            characterData.characterInfoCollect.characterCollect.hp += fillHp;
+
+            if (characterData.characterInfoCollect.characterCollect.hp > characterData.characterInfoCollect.characterCollect.maxHp)
+            {
+                characterData.characterInfoCollect.characterCollect.hp = characterData.characterInfoCollect.characterCollect.maxHp;
+
+            }
+            GameManager.Instance.ingameTopUI.nowHpText.text = characterData.characterInfoCollect.characterCollect.hp + "/" + characterData.characterInfoCollect.characterCollect.maxHp.ToString();
+            GameManager.Instance.playerControler.ironclad.Heal(0);
+        }
+
+        private void OnClericEvent(Data.CharacterData characterData)
+        {
+            int fillLittleHp = 10;
+
+            GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.hp += fillLittleHp;
+            GameManager.Instance.dataManager.data.characterData.characterInfoCollect.characterCollect.maxHp += fillLittleHp;
+            GameManager.Instance.ingameTopUI.nowHpText.text = characterData.characterInfoCollect.characterCollect.hp + "/" + characterData.characterInfoCollect.characterCollect.maxHp.ToString();
+            GameManager.Instance.playerControler.ironclad.Heal(0);
+        }
+
         // Start is called before the first frame update
 
     }
